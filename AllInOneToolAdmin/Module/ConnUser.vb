7eda1dc8812510ef7c59e8511af8937d35a641e9
Hatta <O2PSoftLabs@gmail.com>
2012-10-10 13:43:03 +0700

    Added Module
    Comit *kissu*

---
+++ AllInOneToolAdmin/Module/ConnUser.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+Module ConnUser
+    'Module Created By O2PSoftlabs
+    Public Function connusers() As SqlConnection
+        Dim dcmduser As New SqlCommand
+        Dim conuser As SqlConnection
+
+        conuser = New SqlConnection("server=195.91.172.114;uid=sa;pwd=ShadowRun007;database=RF_User;")
+        Return conuser
+        conuser = Nothing
+    End Function
+    Public dauser As SqlDataAdapter
+    Public dtuser As New DataTable
+    Public conuserc = ConnUser.connusers
+    Public cmduser As SqlCommand
+    Public rouser As SqlDataAdapter
+    Public scmuser As SqlCommandBuilder
+    Public dsuser As DataSet
+    Public rduser As SqlDataReader
+    Public sdbuser As Data.SqlClient.SqlDataAdapter
+    Public perintahuser As SqlClient.SqlCommand
+    Public sqlManagerAreauser As CurrencyManager
+End Module
---
+++ AllInOneToolAdmin/Module/ConnWorld.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+Module ConnWorld
+    'Module Created By O2PSoftlabs
+    Public Function connworlds() As SqlConnection
+        Dim dcmdworld As New SqlCommand
+        Dim conworld As SqlConnection
+
+        conworld = New SqlConnection("server=195.91.172.114;uid=sa;pwd=ShadowRun007;database=RF_World;")
+        Return conworld
+        conworld = Nothing
+    End Function
+    Public daworld As SqlDataAdapter
+    Public dtworld As New DataTable
+    Public conworldc = ConnWorld.connworlds
+    Public cmdworld As SqlCommand
+    Public riworld As SqlCommand
+    Public roworld As SqlDataAdapter
+    Public scmworld As SqlCommandBuilder
+    Public dsworld As DataSet
+    Public rdworld As SqlDataReader
+    Public sdbworld As Data.SqlClient.SqlDataAdapter
+    Public perintahworld As SqlClient.SqlCommand
+    Public sqlManagerAreaworld As CurrencyManager
+End Module
